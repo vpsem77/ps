1640ae65b90b6571345aedae2c78e47d07cfe79f
vpsem77 <97038784+vpsem77@users.noreply.github.com>
2025-10-07 14:13:18 +0900

    Update and rename Algorithm/combination.cpp to Algorithm/Math/combination.cpp

--- Algorithm/Math/combination.cpp
+++ Algorithm/Math/combination.cpp
@@ -1,6 +1,6 @@
 const int mod = 1e9 + 7;
-const int M = 4000000;
-ll fac[M + 5], infac[M + 5];
+const int NMAX = 4000000;
+ll fac[NMAX + 5], infac[NMAX + 5];
 ll mpow(ll a, ll x) {
     ll res = 1;
     if (x == 0) return 1;
@@ -22,7 +22,7 @@ fac[0] = 1;
 for (int i = 1; i <= M; i++) {
     fac[i] = fac[i - 1] * i % mod;
 }
-infac[M] = mpow(fac[M], mod - 2);
+infac[NMAX] = mpow(fac[NMAX], mod - 2);
 for (int i = M - 1; i >= 0; i--) {
     infac[i] = infac[i + 1] * (i + 1) % mod;
 }
